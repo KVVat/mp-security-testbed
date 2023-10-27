b866106037e99381626be450c2355aa83112f7f2
wkouki <wkouki@google.com>
2023-10-27 15:26:25 +0900

    new project

---
+++ settings.gradle.kts
@@ -0,0 +1,28 @@
+rootProject.name = "TestBed"
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        maven("https://oss.sonatype.org/content/repositories/snapshots/")
+    }
+
+    plugins {
+        kotlin("jvm").version(extra["kotlin.version"] as String)
+        id("org.jetbrains.compose").version(extra["compose.version"] as String)
+        //id("org.jetbrains.kotlin.jvm") version "1.9.0"
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        maven("https://oss.sonatype.org/content/repositories/snapshots/")
+    }
+}
+include(":testbed")
+include(":lib")
